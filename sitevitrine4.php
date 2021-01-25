87ad38e4647f85cbec32874e31b776c7f6b3253b
jcjuanka <77975380+jcjuanka@users.noreply.github.com>
2021-01-25 15:13:13 +0100

    Add files via upload

---
+++ sitevitrine4.php
@@ -0,0 +1,611 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+       <meta charset= "UTF-8"/>
+       <link rel= "stylesheet" type= "text/css" href= "sitevitrine4.css">
+       <link rel="stylesheet" media="screen and (max-width: 1280px)" href="petite_resolution.css"/>
+       <title>SITE VITRINE4</title>
+</head>
+
+<body>
+	<div id="menu"></div>
+<header>		
+<nav>
+	<ul>
+		<li class="menu"><a href="#menu"><img class="bola" src="bolajira.gif" alt="photo"/></a><br>
+		</li>
+		<li class="apropo"><a href="a-propo-du-site.html" target="_blank"><div class="resto">SITE DE INFORMATION</div></a><br>
+		</li>
+		<li class="actualite"><a href="#menu"><img class="loupe" src="loupeseule.jpg" alt="photo"/></a><br>	
+        <ul class="derrulant">
+        </li>
+
+		<li class="derru"><a href="#menu">ACCUEIL</a></li>
+        <ul class="derrulant"></ul>
+      	    <li><a href="#">information du site</a></li>
+		 	<li><a href="#">a qui est destine site</a></li>
+		 	<li><a href="#">comment devenir membre</a></li>
+		</li>
+
+		<li class="derr"><a href="#services">SERVICES</a></li>
+        <ul class="derrulant"></ul>
+      	    <li><a href="#">site</a></li>
+		 	<li><a href="#">site</a></li>
+		 	<li><a href="#">membre</a></li>
+		</li>
+
+		<li class="der"><a href="#apropo">A PROPO</a></li>
+        <ul class="derrulant"></ul>
+      	    <li><a href="#">information</a></li>
+		 	<li><a href="#">a qui</a></li>
+		 	<li><a href="#">comment</a></li>
+		</li>
+
+		<li class="d"><a href="#projets">PROJETS</a></li>
+		<ul class="derrulant"></ul>
+      	    <li><a href="#">information</a></li>
+		 	<li><a href="#">a qui</a></li>
+		 	<li><a href="#">comment</a></li>
+		 	<li class="de"><a href="#contact">CONTACT</a>
+		</li>
+	</ul>
+</li></ul></li></ul></ul></li></ul></li></ul>
+</nav>
+</header>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<!--<a href= "#"><div class= "utilisateurs">CARLOS FLOREZ</div></a><br/>-->
+<section class="contacto"><br>
+<div id="contact"></div><br><br><br><br>	
+<div>CONTACT</div>
+<p>
+Nous écrirer,
+Pour toute question, appelez-nous au : 01 02 03 04 05 
+
+Envoyer
+Adresse
+00, rue du Paris
+75001 Paris, France<br>
+info@hotmail.fr
+Tél : 01 02 03 04 05
+<br><br>
+Offres d'emploi
+Pour postuler, envoyez votre C.V à l'adresse e-mail suivant : info@monsite.fr
+<br><br>
+Devis gratuit : 01 02 03 04 05
+</p>
+</section><br>
+
+
+<!--<a href= "#"><div class= "utilisateurs">RAUL PEREZ</div></a><br/>-->
+<section class="services">
+<div id="servi"></div><br><br><br><br>	
+<div id="services"></div><br><br><br><br>	
+<div>SERVICES</div>
+<p>
+<strong>COACHING COMMUNICATION</strong><br>
+Nous sommes fiers de mettre à votre disposition des <strong>methodes</strong> qui vous permettront de créer de magnifiques Projets, Sites,  ou Autres Domaines,<br><br>
+
+<strong>STRATÉGIE</strong><br>
+1 Présentez votre entreprise, vos services et vos équipes.<br>
+2 Soulignez vos avantages par rapport à vos concurrents.<br>
+3 Démarquez-vous et captivez l'attention de vos visiteurs.<br><br>
+
+<strong>GESTION</strong><br>
+1 Équipez-vous du Matériel Adéquate a votre Activite.<br>
+2 Organiser toujours et a tout moment vos Affaires.<br>
+3 Faites une Liste.<br><br>
+
+CONTRÔLE HUILE ET FREINS<br>
+MÉCANIQUES DÉDIÉS<br>
+CONTRÔLE DES VÉHICULES<br>
+RÉPARATION<br>
+Service de réparation de carrosserie, travaux de mécanique
+
+REVÊTEMENT<br>
+Revêtement intérieur, moquettes acoustiques et accessoires automobiles
+
+PEINTURE<br>
+Service d'entretien et de peinture pour carrosserie et jantes. Choix de teintes métallisées<br><br>
+
+GESTION DE PROJET
+1 h
+220 euros
+220 €
+Réserver
+
+COMMUNICATION
+1 h
+150 euros
+150 €
+Réserver
+
+COACHING STRATÉGIE
+1 h
+170 euros
+170 €
+Réserver</p>
+
+<!--<div id="actualite"></div><br><br>-->	
+<p>MENU</p>
+<p>
+Mettez vos clients en appétit. Décrivez en quelques mots votre menu ici.
+<br>
+ENTRÉES
+Section de votre menu. Ajoutez une courte description.
+Salade de tomates
+Ceci est un élément de votre menu. Ajoutez une courte description.
+9,00 €<br>
+Salade verte composée
+Ceci est un élément de votre menu. Ajoutez une courte description.
+9,00 €<br>
+Salade César au choux kale
+Ceci est un élément de votre menu. Ajoutez une courte description.
+9,00 €<br>
+Chou-fleur grillé
+Ceci est un élément de votre menu. Ajoutez une courte description.
+9,00 €
+<br><br>
+Réserver un cours en ligne
+Paragraphe. Cliquez ici pour ajouter votre propre texte. Utilisez cet espace pour relater votre parcours et présenter votre activité à vos visiteurs.
+
+​Commencer 
+Introduction
+En lire plus
+45 Min
+Gratuit
+Réserver
+
+Français
+En lire plus
+45 Min
+39 euros
+39 €
+Réserver
+
+Sciences
+En lire plus
+45 Min
+39 euros
+39 €
+Réserver
+
+Histoire
+En lire plus
+45 Min
+39 euros
+39 €
+Réserver
+
+Math
+En lire plus
+45 Min
+39 euros
+39 €
+Réserver<br><br>
+
+SOINS VISAGE
+RÉSERVER<br> 
+
+MAQUILLAGE
+RÉSERVER<br>
+
+CHEVEUX
+RÉSERVER<br>
+
+ONGLES
+RÉSERVER<br><br>
+
+Prestation complète
+Paragraphe. Cliquez ici pour ajouter votre propre texte. Utilisez cet espace pour relater votre parcours et présenter votre activité à vos visiteurs.
+
+Prestation partielle
+Le jour J
+Séminaires de loisirs
+Fêtes d'entreprise
+Conférences
+Événements privés
+</p>
+</section><br>
+
+<!--<a href= "#"><div class= "utilisateurs">LUIZ SANCHEZ</div></a><br/>-->
+<section class="entreprise">
+
+<p><img src="bolajira.gif" alt="photo"/></p>
+
+<section class="membre">
+Utilisez cet espace pour décrire votre activité. 
+Expliquez à vos visiteurs<br>	
+<?php
+ $adulte = true; //LES CONDITIONS BOOLEAN
+ if ($adulte) //si adulte 
+ {
+ 	echo 'vous est adulte';
+ }
+ else
+ {
+ 	echo 'vous est mineur';
+ }
+?><br/> 
+
+<?php
+ $VousAvaisMange = false; //LES CONDITIONS BOOLEAN
+ if ($VousAvaisMange) //si .........
+ {
+ 	echo ' n\'avais pas faim';
+ }
+ else
+ {
+ 	echo ' avais faim';
+ }
+?><br/> 
+
+<?php
+ $VousAvaisUneVeste = true; //LES CONDITIONS BOOLEAN
+ if ($VousAvaisUneVeste) //si ......... 
+ {
+ 	echo 'n\'avais pas froid';
+ }
+ else
+ {
+ 	echo 'avais froid';
+ }
+?><br/> 
+
+<?php
+ $VousAvaisUneVoiture = true; //LES CONDITIONS BOOLEAN
+ if ($VousAvaisUneVoiture) //si ......... 
+ {
+ 	echo 'vous est en voiture';
+ }
+ else
+ {
+ 	echo 'vous n\'est pas en voiture';
+ }
+?><br/> 
+
+<?php
+ $VousAvaisLheure = true; //LES CONDITIONS BOOLEAN
+ if ($VousAvaisLheure) //si ......... 
+ {
+ 	echo 'il est midi 35';
+ }
+ else
+ {
+ 	echo 'jai pas lheure desole';
+ }
+?><br/> 
+
+<?php
+ $VousAvaisUneFemme = false; //LES CONDITIONS BOOLEAN
+ if ($VousAvaisUneFemme) //si 
+ {
+ 	echo 'vous est marie';
+ }
+ else
+ {
+ 	echo 'vous est celibataire';
+ }
+?><br/>       
+
+
+<?php
+$prenom = array('ariana', 'carilo', 'riel','ariana', 'carilo', 'riel'); //BOUCLE FOREACH pour afficher  (des tres grand tableaux)
+$prenom1 = array('riana', 'arilo', 'ariel');
+
+foreach ($prenom1 as $prenom1) //affiche array($prenom1)
+{
+     echo '<p>' . $prenom1 . '</p>';//affiche array($prenom1)
+}	
+?>
+</section>
+
+<div id="apropo"></div><br><br><br><br>	
+<div>A PROPOS</div>
+<p>
+DEPUIS PLUS DE 30 ANS !
+Paragraphe.
+En soutien aux petits commerces, nous avons pris la desicion de faire de la Communication, afin de developper leur entreprice.<br><br>
+<strong>METODE</strong><br> 
+1 Expliquez présentez votre "Activité" "vos motivations" et "votre parcours".<br> 
+2 Ajouter votre propre "Experience" dans votre Activite.<br>  
+3 Ajouter votres "Reusites" et "Inconveniants".<br>
+  
+<h6>IL SUFI DE PARTAGER VOUS CONNAISANCES AVEC NOUS</h6> 
+<h6>COMMENT TOUS LES AUTRES MEMBRES DU SITE</h6>
+</p>
+</section><br>
+
+<section class="projets">
+<div id="projets"></div><br><br><br><br>	
+<div>PROJETS</div>
+
+<div>CLICK SUR L'IMAGE DE VOTRE PROJET</div>
+<div class="photoprojets">
+<p><a href= "servicessitevitrine4.php#entrepreneur"><img src="resto.jfif" alt="photo"></a></p>	
+<p><a href= "servicessitevitrine4.php#batiment"><img src="batiment.jpg" alt="photo"></a></p>	
+<p><a href= "servicessitevitrine4.php#commerce"><img src="modeleplan.jpeg" alt="photo"></a></p>	
+<p><a href= "servicessitevitrine4.php#restauration"><img src="platsaumon.jpeg" alt="photo"></p>
+<p><a href= "servicessitevitrine4.php#entrepreneur"><img src="girl.jpg" alt="photo"></a></p>	
+<p><a href= "servicessitevitrine4.php#internet"><img src="loading.gif" alt="photo"></a></p>	
+<p><a href= "servicessitevitrine4.php#entrepreneur"><img src="pain.jpg" alt="photo"></a></p>	
+<p><a href= "servicessitevitrine4.php#entrepreneur"><img src="noel.gif" alt="photo"></a></p>
+<p><a href= "servicessitevitrine4.php#internet"><img src="maya.jpg" alt="photo"></a></p>	
+<p><a href= "servicessitevitrine4.php#commerce"><img src="poignedemain.jfif" alt="photo"></a></p>	
+<p><a href= "servicessitevitrine4.php#restauration"><img src="table.jpg" alt="photo"></a></p>	
+<p><a href= "servicessitevitrine4.php#batiment"><img src="grue.jfif" alt="photo"></a></p>
+<p><a href= "servicessitevitrine4.php#commerce"><img src="business-info.jpg" alt="photo"></a></p>	
+<p><a href= "servicessitevitrine4.php#internet"><img src="programmes-windows.jpg" alt="photo"></a></p>	
+<p><a href= "servicessitevitrine4.php#restauration"><img src="seveurrestaurant.jpeg" alt="photo"></a></p>	
+<p><a href= "servicessitevitrine4.php#batiment"><img src="btpplombier.jpg" alt="photo"></a></p>
+</div>	
+<p>		
+En Soutien aux petits commerces <em>Images</em>: Demande un "geste patriotique" aux Français
+Dix millions de pauvres en 2020,
+</p>
+<p>LOCAUX</p>	
+<p>GALERIE</p>
+<!--<div id="programme"></div><br><br>-->	
+<p>PROGRAMME</p>
+<p>
+conférenc++​.
+LUNDI
+​9h00 - 12h00
+
+Paragraphe. Cliquez ici pour ajouter votre propre texte.
+
+​12h15 - 15h00
+
+Paragraphe. Cliquez ici pour ajouter votre propre texte.
+
+15h30 - 18h15
+
+Paragraphe. Cliquez ici pour ajouter votre propre texte.div
+​18h30 - 20h00
+
+Paragraphe. Cliquez ici pour ajouter votre propre texte.
+
+MARDI
+​9h00 - 12h00
+
+Paragraphe. Cliquez ici pour ajouter votre propre texte.
+
+​12h15 - 15h00
+
+Paragraphe. Cliquez ici pour ajouter votre propre texte.
+
+​18h30 - 20h00
+
+Paragraphe. Cliquez ici pour ajouter votre propre texte.
+
+15h30 - 18h15
+
+Paragraphe. Cliquez ici pour ajouter votre propre texte.
+
+MERCREDI
+​9h00 - 12h00
+
+Paragraphe. Cliquez ici pour ajouter votre propre texte.
+
+​12h15 - 15h00
+
+Paragraphe. Cliquez ici pour ajouter votre propre texte.
+
+15h30 - 18h15
+
+Paragraphe. Cliquez ici pour ajouter votre propre texte.
+
+​18h30 - 20h00
+
+Paragraphe. Cliquez ici pour ajouter votre propre texte.
+
+2023 par Conférenc++. Créé avec .com
+</p>
+
+<!--<div id="actualite"></div><br><br>-->	
+<p>ADHÉSION</p>
+<p>
+Devenez membre !
+
+Un bureau génial
+
+5 x 1 heure gratuite en
+
+salle de réunion
+
+Accès à une communauté géniale d'entrepreneurs et de créatifs
+
+Internet haut débit 2GB
+
+Appels Illimités vers l'Étranger
+
+Happy hour tous les vendredis dès 18h, alcool gratuit
+</p>
+
+<!--<div id="actualite"></div><br><br>-->	
+<p>NOTRE COMMUNAUTÉ</p>
+<p>ESPACES</p>
+<p>
+RÉSERVEZ EN LIGNE Prestation « Personnalisez ma voiture »
+2 h
+100 euros
+100 €
+RÉSERVER
+</p>
+
+<!--<div id="actualite"></div><br><br>-->	
+<p>NOTRE EQUIPE</p>
+<p>
+Partenaire
+Marie
+Garnier
+Paragraphe. Cliquez ici pour ajouter votre propre texte. C'est très simple.
+info@monsite.fr
+01 02 03 04 05
+LinkedIn
+Twitter
+
+Associé principal
+Jean
+Martin
+Paragraphe. Cliquez ici pour ajouter votre propre texte. C'est très simple.
+info@monsite.fr
+01 02 03 04 05
+LinkedIn
+Twitter
+
+Associée
+Julie
+Brun
+Paragraphe. Cliquez ici pour ajouter votre propre texte. C'est très simple.
+info@monsite.fr
+01 02 03 04 05
+</p>
+<!--<div id="actualite"></div><br><br>-->	
+<p>ESPACE MEMBRES</p>
+<p>
+ENTRE VOTRE MOT DE PASS
+ICI VOUS POUVAIS COMMUNIQUER AVEC LES MEMBRES SUR LE MEME SUJET ACTIVITE 
+</p>
+
+<div id="actualite"></div><br><br><br><br>
+<div>ACTUALITES</div>
+<p>
+Les nouvelles tendances
+Prochaines rénovations
+Nos secrets d'intérieur
+L'équipe féminine de robotique remporte le prix	
+	
+11h46Coronavirus : la situation dans le monde, le jeudi 12 janvier, les pays les plus touchés
+11h38Covid-19  
+</p>
+<!--<div id="actualite"></div><br><br>-->	
+<p>TITRES</p>
+<p>	
+MEMBRES
+ACCUEIL
+FORUM
+BLOG
+MEMBRES
+À PROPOS
+CONTACT
+</p>
+<!--<div id="actualite"></div><br><br>-->
+<p>MENU</p>
+<p>
+BAGEL NATURE
+BAGEL NATURE
+Ceci est un élément de votre menu. Ajoutez une courte description.
+9,00 €
+BAGEL AU SÉSAME
+BAGEL AU SÉSAME
+Ceci est un élément de votre menu. Ajoutez une courte description.
+9,00 €
+BAGEL AUX GRAÎNES DE TOURNESOL
+BAGEL AUX GRAÎNES DE TOURNESOL
+Ceci est un élément de votre menu. Ajoutez une courte description.
+9,00 €
+</p>
+
+<p>Événements</p>
+<p>
+23
+sam.
+sept.
+10 000 € contre le cancer du sein
+Paris
+S'inscrire
+
+20
+lun.
+nov.
+Les femmes ingénieurs enseignent le Javascript
+Paris
+S'inscrire
+
+19
+mar.
+déc.
+Conférence : Discrimination salariale
+Paris
+S'inscrire
+</p>
+
+<p>Fournisseurs</p>	
+<p>
+Fournisseurs recommandés
+
+Traiteur
+Nom
+Cliquez ici pour ajouter votre propre texte.
+10, rue du Paris
+75005 Paris, France
+info@monsite.fr
+Tél : 01 02 03 04 05
+
+Fleuriste
+Nom
+Cliquez ici pour ajouter votre propre texte.
+10, rue du Paris
+75005 Paris, France
+info@monsite.fr
+Tél : 01 02 03 04 05
+
+Location
+Nom
+Cliquez ici pour ajouter votre propre texte.
+10, rue du Paris
+75005 Paris, France
+info@monsite.fr
+Tél : 01 02 03 04 05
+
+Éclairage
+Nom
+Cliquez ici pour ajouter votre propre texte.
+10, rue du Paris
+75005 Paris, France
+info@monsite.fr
+Tél : 01 02 03 04 05
+
+Organisateur
+Nom
+Cliquez ici pour ajouter votre propre texte.
+10, rue du Paris
+75005 Paris, France
+info@monsite.fr
+Tél : 01 02 03 04 05
+
+Animation
+Nom
+Cliquez ici pour ajouter votre propre texte.
+10, rue du Paris
+75005 Paris, France
+info@monsite.fr
+Tél : 01 02 03 04 05
+</p>
+<figure>
+<img src="corazones.gif" alt="photo"/>
+<img src="girl.jpg" alt="photo"/>
+<img src="pagina.jpg" alt="photo"/>
+<a href= "#projets"><img src="bolajira.gif" alt="photo"/>
+</figure>
+</section>
+
+
+<footer>
+ 
+Copyright site - Tous droits reserves  
+</footer>
+
+</body>
+</html>
+
+
+
+
